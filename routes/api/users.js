6eb72c7d436dabbde9c403d67fa617871a3c41b4
Kushagra <kus.amigo@gmail.com>
2020-07-26 22:03:44 +0530

    Check if user exists validation added to Register User Api

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/Users');
+
 // @route   POST api/users
 // @desc    Register Useer
 // @access  Public
@@ -15,12 +17,26 @@ router.post(
       'Please enter a password with 6 ir more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Users route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      res.send('Users route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 

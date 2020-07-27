f7e5b64c2220e5df1486ddc3f684f428f4896b39
Kushagra <kus.amigo@gmail.com>
2020-07-27 11:02:37 +0530

    Encrypted password and added email for Register User Api

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/Users');
 
@@ -29,10 +31,31 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      res.send('Users route');
+      const avatar = gravatar.url(email, {
+        sg: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('Users registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');

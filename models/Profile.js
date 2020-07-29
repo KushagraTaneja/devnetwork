a12fcbb6a0eba4a51b6db3754a7a75efe9ddba95
Kushagra Taneja <50551987+KushagraTaneja@users.noreply.github.com>
2020-07-29 20:08:36 +0530

    get user profile api created

--- models/Profile.js
+++ models/Profile.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const Users = require('./Users');
 
 const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: user,
+    ref: Users,
   },
   company: {
     type: String,

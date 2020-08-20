0d47d1cd0da1c4a2dfdca8fa0d80f5213cd4d443
Kushagra Taneja <50551987+KushagraTaneja@users.noreply.github.com>
2020-08-20 08:38:37 +0530

    Started work on dashboard

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -23,7 +23,6 @@ export default function (state = initialState, action) {
         ...state,
         error: payload,
         loading: false,
-        profile: null,
       };
     default:
       return state;
